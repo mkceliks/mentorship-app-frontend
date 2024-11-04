15589d2c0922a5ae0c3783c5fca2fccff96e843e
Mustafa Kemal Çelik <66789068+mkceliks@users.noreply.github.com>
2024-11-04 23:36:30 +0400

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_STAGE === "staging"
-    ? process.env.REACT_APP_STAGING_API_URL
-    : process.env.REACT_APP_PROD_API_URL;
+const API_BASE_URL = process.env.REACT_APP_API_URL
 
 export default API_BASE_URL;
 
