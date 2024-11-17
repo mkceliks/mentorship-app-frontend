939a3548897ffdb3d1b207b4b68ac20eedca84c2
Mustafa Kemal Çelik <66789068+mkceliks@users.noreply.github.com>
2024-11-17 14:57:25 +0400

    api updates for register

--- src/components/Authentication/handlers/handleRegister.js
+++ src/components/Authentication/handlers/handleRegister.js
@@ -1,22 +1,35 @@
 import { registerUser } from "../../../api/auth/register";
 
-export const handleRegister = async (email, password, selectedFile, setMessage) => {
+export const handleRegister = async (email, password, name, role, selectedFile, setMessage) => {
     if (!selectedFile) {
         alert("Please select a file to upload.");
         return;
     }
 
     try {
-        const response = await registerUser(email, password, selectedFile);
+        const selectedFileData = {
+            file: selectedFile,
+            base64File: await fileToBase64(selectedFile),
+        };
+
+        const response = await registerUser(email, password, name, role, selectedFileData);
 
         if (response.ok) {
             setMessage("Registration successful! Please log in.");
         } else {
             const errorText = await response.text();
-            setMessage(`Failed to upload file: ${errorText}`);
+            setMessage(`Failed to register: ${errorText}`);
         }
     } catch (error) {
         console.error("Error during registration:", error);
         setMessage("Registration failed. Please try again.");
     }
 };
+
+const fileToBase64 = (file) =>
+    new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+        reader.readAsDataURL(file);
+    });
