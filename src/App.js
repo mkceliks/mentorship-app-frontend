0e07a4967ef80253d37d78e9e2228917e8347b6f
Mustafa Kemal Çelik <66789068+mkceliks@users.noreply.github.com>
2024-11-17 14:14:31 +0400

    sign in and sign up improvements

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import SignIn from "./components/Authentication/SignIn";
+import SignInSide from "./components/Authentication/SignInSide";
+import SignUp from "./components/Authentication/SignUp";
 import { getAccessToken } from "./utils/config";
+// import Navbar from "./components/Navbar/Navbar";
+import FileManagement from "./components/FileManagement/FileManagement";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
     const isAuthenticated = !!getAccessToken();
 
     return (
         <Router>
+            {/* <Navbar /> */}
             <Routes>
-                <Route
+                {/* <Route
                     path="/"
-                    element={!isAuthenticated ? <SignIn /> : <Navigate to="/hello_world" replace />}
+                    element={!isAuthenticated ? <Home /> : <Navigate to="/files" replace />}
+                /> */}
+                <Route
+                    path="/sign-in"
+                    element={!isAuthenticated ? <SignInSide /> : <Navigate to="/files" replace />}
+                />
+                <Route
+                    path="/sign-up"
+                    element={!isAuthenticated ? <SignUp /> : <Navigate to="/files" replace />}
+                />
+                <Route
+                    path="/files"
+                    element={
+                        <ProtectedRoute isAuthenticated={isAuthenticated}>
+                            <FileManagement />
+                        </ProtectedRoute>
+                    }
                 />
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </Router>
     );
 }
-
 export default App;
