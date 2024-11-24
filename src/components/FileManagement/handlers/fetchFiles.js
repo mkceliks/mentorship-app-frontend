4257ccaccb8fbf08206ffda0303497481fc5071c
Mustafa Kemal Çelik <66789068+mkceliks@users.noreply.github.com>
2024-11-24 20:13:21 +0400

    add token func to upload

--- src/components/FileManagement/handlers/fetchFiles.js
+++ src/components/FileManagement/handlers/fetchFiles.js
@@ -1,6 +1,8 @@
 import { listFiles } from "../../../api/s3";
 
 export const fetchFiles = async (setFiles) => {
+    const token = getIdToken();
+
     try {
         const data = await listFiles();
         setFiles(data);
